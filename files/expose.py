0777230aecdd37f9f5cf7eda31496f178d681dff
Jonas Elmesten <jonas_elmesten@hotmail.com>
2021-08-06 10:08:23 +0200

    Some clean up & restructuring.

---
+++ files/expose.py
@@ -0,0 +1,47 @@
+import os
+
+import eel
+
+from classifier.augmentation import extract_class_trees
+from files.csv import update_csv_file
+from folders import get_tsmt, get_gui
+from tree_segmentation.image import flip_img_csv
+
+
+@eel.expose
+def send_to_training(file, csv_data, train_both):
+    if file is None:
+        return
+
+    image_file = get_gui(file)
+    image_name = os.path.basename(image_file)
+
+    csv_file = get_gui(file.split(sep=".")[0] + ".csv")
+    csv_name = os.path.basename(csv_file)
+
+    update_csv_file(csv_file, csv_data)
+
+    if os.path.isfile(image_file) and os.path.isfile(csv_file):
+
+        # Move to segmentation training
+        if train_both:
+            extract_class_trees(image_file, csv_file)
+            flip_img_csv(image_file, csv_file, get_tsmt())
+            #shutil.move(image_file, get_tsmt(image_name))
+            #shutil.move(csv_file, get_tsmt(csv_name))
+            remove_file(image_file)
+        else:
+            extract_class_trees(image_file, csv_file)
+            eel.project_file_remove_confirmed()
+            remove_file(image_file)
+
+    else:
+        raise FileNotFoundError
+
+
+@eel.expose
+def remove_file(file_name):
+    image = file_name
+    csv_file = file_name.split(sep=".")[0] + ".csv"
+    os.remove(get_gui(image))
+    os.remove(get_gui(csv_file))
--- main.py
+++ main.py
@@ -8,52 +8,14 @@ from tkinter import filedialog, Tk
 
 import eel
 
-#from classifier.tree_classifier import extract_class_trees, TreeClassifier
+# from classifier.tree_classifier import extract_class_trees, TreeClassifier
+from classifier.augmentation import extract_class_trees
 from files.csv import update_csv_file, save_csv_file
 from folders import get_gui, get_tsmt, get_c, get_tsm
 from gui.menu import MainMenu
 from gui.window import TreeWindow
 from tree_segmentation.image import flip_img_csv
 from tree_segmentation.segment import TreePredictor
-
-
-@eel.expose
-def send_to_training(file, csv_data, train_both):
-    if file is None:
-        return
-
-    image_file = get_gui(file)
-    image_name = os.path.basename(image_file)
-
-    csv_file = get_gui(file.split(sep=".")[0] + ".csv")
-    csv_name = os.path.basename(csv_file)
-
-    update_csv_file(csv_file, csv_data)
-
-    if os.path.isfile(image_file) and os.path.isfile(csv_file):
-
-        # Move to segmentation training
-        if train_both:
-            extract_class_trees(image_file, csv_file)
-            flip_img_csv(image_file, csv_file, get_tsmt())
-            #shutil.move(image_file, get_tsmt(image_name))
-            #shutil.move(csv_file, get_tsmt(csv_name))
-            remove_file(image_file)
-        else:
-            extract_class_trees(image_file, csv_file)
-            eel.project_file_remove_confirmed()
-            remove_file(image_file)
-
-    else:
-        raise FileNotFoundError
-
-
-@eel.expose
-def remove_file(file_name):
-    image = file_name
-    csv_file = file_name.split(sep=".")[0] + ".csv"
-    os.remove(get_gui(image))
-    os.remove(get_gui(csv_file))
 
 
 class Application:
@@ -72,8 +34,8 @@ class Application:
 
         print("Loading models...")
 
-        #classifier = TreeClassifier()
-        #classifier.load_model(get_c("ep_20_no_r.h5"))
+        # classifier = TreeClassifier()
+        # classifier.load_model(get_c("ep_20_no_r.h5"))
         classifier = None
         predictor = TreePredictor()
 
@@ -82,9 +44,12 @@ class Application:
     def create_main_window(self):
         return MainMenu(run_prediction=self.run_prediction,
                         add_images=self.copy_images,
-                        annotate_prediction=self.open_annotation)
+                        annotate_prediction=self.open_annotation_window)
 
     def copy_images(self):
+        """
+        Opens a file dialog to select images to be copied.
+        """
         root = Tk()
         root.title("Select training images")
         root.withdraw()
@@ -94,7 +59,12 @@ class Application:
             copy2(file, get_gui())
 
     def get_remaining_time(self, time_taken, images_left):
-
+        """
+        Creates an estimated of the time left for the remaining images.
+        @param time_taken: List of time samples.
+        @param images_left: Total images left.
+        @return: Estimate of remaining time.
+        """
         if len(time_taken) == 0:
             return "∞"
 
@@ -105,8 +75,13 @@ class Application:
 
         return str(remaining_min) + ":" + str(remaining_sec)
 
-    # TODO: Create application logic module to simplify
-    def run_prediction(self):
+    def run_prediction(self, bounding_box_threshold=0.40):
+        """
+        Runs the tree bounding box & species prediction.
+        Will also display the estimated time left if there is a big amount of images to predict on.
+
+        For each tree: [(x, y, x, y), Class, Score]
+        """
 
         images = []
         time_taken = deque(maxlen=10)
@@ -122,7 +97,7 @@ class Application:
 
         tree_data = dict()
 
-        # Predict & classify each image
+        # Predict & classify each image.
         for index, image_path in enumerate(images, start=1):
 
             start_time = time.perf_counter()
@@ -130,12 +105,12 @@ class Application:
             csv_path = image_path.split(sep=".")[0] + ".csv"
             tree_data[image_path] = list()
 
-            # TODO: How does this effect prog. bar?
+            # Image has already been predicted on earlier.
             if os.path.exists(csv_path):
                 print("Already exists:", os.path.basename(csv_path))
                 continue
 
-            # Update GUI
+            # Update time left.
             if index == 1:
                 remaining_time = self.get_remaining_time(time_taken, image_count)
             elif index > 6:
@@ -149,24 +124,23 @@ class Application:
                                          + remaining_time)
             self.main_window.inc_progress_bar(inc_value)
 
-            # Predict & classify
-            img_list, bb_list = self.predictor.predict_trees(image_path=image_path, score_threshold=0.40)
+            # Predict tree bounding boxes.
+            img_list, bb_list = self.predictor.predict_trees(image_path=image_path,
+                                                             score_threshold=bounding_box_threshold)
             result_list = []
 
             # TODO: Make classifier work.
             for i in range(len(img_list)):
                 result_list.append(["Tree", 0.73] * len(img_list))
             print(result_list)
-            #result_list = self.classifier.classify_batch_of_trees(img_list, batch_size=len(img_list))
+            # result_list = self.classifier.classify_batch_of_trees(img_list, batch_size=len(img_list))
             # TODO: --------------------
 
-            # Start assembling final data list
+            # Start assembling final data list.
             for i, coordinate in enumerate(bb_list):
                 tree_data[image_path].append(list(coordinate))
                 tree_data[image_path][i].append(result_list[i][0])
                 tree_data[image_path][i].append(result_list[i][1])
-
-                # For each tree: [(x, y, x, y), Class, Score]
 
             stop_time = time.perf_counter() - start_time
             time_taken.append(stop_time)
@@ -183,17 +157,26 @@ class Application:
 
         self.create_main_window()
 
-    def show_tree_class_prediction(self, tree_data):
+    def show_tree_class_prediction(self, tree_data, accuracy_threshold):
+        """
+        Opens up a window for the user to remove trees that was wrongfully predicted.
+        This makes the last modifications in the via.html-view easier and it helps with
+        showing an overview how well the current model can predict.
 
+        For each tree: [(x, y, x, y), Class, Score]
+
+        @param tree_data: Tree species prediction data.
+        @param accuracy_threshold: Trees with prediction above this threshold won't be shown.
+        """
         # TODO: Jump over empty images
-        # TODO: Continue on one page per sort: 10 images all the time, show total of left for spieces.
+        # TODO: Continue on one page per sort: 10 images all the time, show total of left for species.
 
         tree_species = {}
 
-        # Group up trees according to species
+        # Group up trees according to species.
         for image_path, tree_list in tree_data.items():
 
-            # Goes through all classes and images
+            # Goes through all classes and images.
             for tree in sorted(tree_list, key=lambda i: i[5]):
 
                 class_name = tree[4]
@@ -203,24 +186,22 @@ class Application:
 
                 accuracy = tree[5]
 
-                if accuracy > 80.0:
+                # Continue if the prediction was above the threshold.
+                if accuracy >= accuracy_threshold:
                     continue
 
                 tree_species[class_name].append([image_path, tree])
 
-        # Open a new window for each species
+        # Open a new window for each species for the user to go through.
         for class_name, trees in tree_species.items():
             tree_window = TreeWindow(class_name, trees)
             tree_window.mainloop()
 
-        print(tree_data)
-
-        # Save all prediction data as csv
+        # Save all prediction data as csv.
         for image_path, tree_list in tree_data.items():
             save_csv_file(image_path, get_gui(), tree_list)
 
-    # TODO: Ladda alla bilder här
-    def open_annotation(self):
+    def open_annotation_window(self):
         eel.start("via.html", mode="mozilla")
 
 
